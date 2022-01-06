6552c81f5ab2b5a8f59e5b1ae3bfc9ce8baff91f
jinsangYoo <jinsang@nhndata.com>
2022-01-06 11:21:14 +0900

    네비게이션, 드로어, 리덕스 관련 플러그인 추가 - 안드로이드 정상 빌드 되도록 수정

--- android/app/src/main/java/com/rnfornhndata/MainApplication.java
+++ android/app/src/main/java/com/rnfornhndata/MainApplication.java
@@ -6,12 +6,16 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -35,6 +39,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+          @Override
+          protected JSIModulePackage getJSIModulePackage() {
+              return new ReanimatedJSIModulePackage();
+          }
       };
 
   @Override
