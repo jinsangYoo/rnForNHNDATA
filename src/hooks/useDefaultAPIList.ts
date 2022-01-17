872a753806b7a22d3183f5e18175833d731892a3
jinsang <jinsang@nhndata.com>
2022-01-17 18:46:15 +0900

    Grid data array에 Record 타입 활용 - API 구현여부 유연성 반영

--- src/hooks/useDefaultAPIList.ts
+++ src/hooks/useDefaultAPIList.ts
@@ -2,30 +2,30 @@ import {useMemo, useState} from 'react'
 import type {IAPI} from '../data/IAPI'
 import * as F from '../data/faker'
 
-const createAPI = (name: string) => {
+const createAPI = (node: Record<string, boolean>) => {
   return {
     id: F.randomId(),
-    name: name,
-    avatar: F.randomAvatarUrl(name),
-    isEnable: false,
+    name: Object.keys(node)[0],
+    avatar: F.randomAvatarUrl(Object.keys(node)[0]),
+    isEnable: Object.values(node)[0],
   } as IAPI
 }
 
 export const useDefaultAPIList = (deps: any[] = []) => {
-  const [name, setName] = useState<string[]>([
-    'Add Cart',
-    'Appear Product',
-    'BUY',
-    'Del Cart',
-    'Event',
-    'Join',
-    'Leave',
-    'Link',
-    'Login',
-    'Push',
-    'Referrer',
-    'Search',
-    'Tel',
+  const [name, setName] = useState<Record<string, boolean>[]>([
+    {'Add Cart': true},
+    {'Appear Product': false},
+    {BUY: false},
+    {'Del Cart': false},
+    {Event: false},
+    {Join: false},
+    {Leave: false},
+    {Link: false},
+    {Login: false},
+    {Push: false},
+    {Referrer: false},
+    {Search: false},
+    {Tel: false},
   ])
   return useMemo(() => {
     return name.map(item => createAPI(item))
